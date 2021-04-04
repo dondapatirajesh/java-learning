b0e36deecec7e42bc7609421b426e38a0aa6493b
dondapatirajesh <chowdaryrajesh1998@gmail.com>
2021-04-04 17:44:28 +0530

    adding new project

---
+++ spring-rest-demo/src/main/java/com/rajesh/springdemo/config/MySpringMvcDispatherServletInitializer.java
@@ -0,0 +1,23 @@
+package com.rajesh.springdemo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { DemoAppConfig.class };
+	}
+
+	// maps to root path
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+
+	}
+}
